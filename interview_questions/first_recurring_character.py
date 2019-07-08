aab29dd4c53d0725032e37b97aa55f996e09132c
NickBenevento <Nicholas.benevento@comcast.net>
2019-07-08 15:10:21 -0400

    formatting

--- interview_questions/first_recurring_character.py
+++ interview_questions/first_recurring_character.py
@@ -4,9 +4,7 @@ def recurring (string):
         if letters.has_key(letter):
             return letter
         letters[letter] = 1
-
     return None
 
-print("hello")
 string = "abcdefd"
 print("first recurring character in %s is %s" % (string, recurring(string)))
