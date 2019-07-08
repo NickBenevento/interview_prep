80fa994e535ad87d006709b3a862fb5f02a5ddc5
NickBenevento <Nicholas.benevento@comcast.net>
2019-07-08 13:20:02 -0400

    add first recurring character in a string question and solution

---
+++ interview_questions/first_recurring_character.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class first_recurring_character {
+
+	public static void main(String[] args) {
+		String string = "abcdef";
+		System.out.print("First recurring character of '" + string + "' is: ");
+		System.out.println(first_char(string));
+	}
+
+	public static Character first_char(String string) {
+		HashMap<Character, Integer> map = new HashMap<>();		 
+		for (int i = 0; i < string.length(); i++) {
+			char letter = string.charAt(i);
+			if (map.containsKey(letter)) {
+				return letter;
+			}
+			map.put(letter, 1);
+		}
+		return null;
+	}
+}
